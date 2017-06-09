05b948a2f54345df8d5bfe85178b499b1bad7cd1
Weilin Luo <william_luo2004@yahoo.com>
2017-06-09 16:06:55 -0700

    Spinning cube: single cube spinning

--- src/spinnycube_my/spinnycube_my.cpp
+++ src/spinnycube_my/spinnycube_my.cpp
@@ -22,12 +22,13 @@
  */
 
 #include <sb7.h>
+#include <vmath.h>
 
-class singlepoint_app : public sb7::application
+class spinnycube_my_app : public sb7::application
 {
     void init()
     {
-        static const char title[] = "OpenGL SuperBible - Single Triangle";
+        static const char title[] = "OpenGL SuperBible - Spinning Cube";
 
         sb7::application::init();
 
@@ -36,29 +37,41 @@ class singlepoint_app : public sb7::application
 
     virtual void startup()
     {
-        static const char * vs_source[] =
-        {
-            "#version 420 core                                                 \n"
-            "                                                                  \n"
-            "void main(void)                                                   \n"
-            "{                                                                 \n"
-            "    const vec4 vertices[] = vec4[](vec4( 0.25, -0.25, 0.5, 1.0),  \n"
-            "                                   vec4(-0.25, -0.25, 0.5, 1.0),  \n"
-            "                                   vec4( 0.25,  0.25, 0.5, 1.0)); \n"
-            "                                                                  \n"
-            "    gl_Position = vertices[gl_VertexID];                          \n"
+		static const char * vs_source[] =
+		{
+			"#version 420 core                                                 \n"
+			"                                                                  \n"
+			"in vec4 position;													\n"
+			"                                                                  \n"
+			"out VS_OUT															\n"
+			"{                                                                 \n"
+			"	vec4 color;														\n"
+			"} vs_out;                                                          \n"
+			"                                                                  \n"
+			"uniform mat4 mv_matrix;											\n"
+			"uniform mat4 proj_matrix;											\n"
+			"                                                                  \n"
+			"void main(void)                                                   \n"
+			"{                                                                 \n"
+			"	gl_Position = proj_matrix * mv_matrix * position;              \n"
+			"	vs_out.color = position * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);		\n"
             "}                                                                 \n"
         };
 
-        static const char * fs_source[] =
-        {
-            "#version 420 core                                                 \n"
-            "                                                                  \n"
-            "out vec4 color;                                                   \n"
-            "                                                                  \n"
-            "void main(void)                                                   \n"
-            "{                                                                 \n"
-            "    color = vec4(0.0, 0.8, 1.0, 1.0);                             \n"
+		static const char * fs_source[] =
+		{
+			"#version 420 core                                                 \n"
+			"                                                                  \n"
+			"out vec4 color;                                                   \n"
+			"                                                                  \n"
+			"in VS_OUT															\n"
+			"{                                                                 \n"
+			"	vec4 color;														\n"
+			"} fs_in;                                                          \n"
+			"                                                                  \n"
+			"void main(void)                                                   \n"
+			"{                                                                 \n"
+			"    color = fs_in.color;											\n"
             "}                                                                 \n"
         };
 
@@ -76,28 +89,140 @@ class singlepoint_app : public sb7::application
 
         glLinkProgram(program);
 
+		mv_location = glGetUniformLocation(program, "mv_matrix");
+		proj_location = glGetUniformLocation(program, "proj_matrix");
+
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
+
+		static const GLfloat vertex_positions[] =
+		{
+			-0.25f,  0.25f, -0.25f,
+			-0.25f, -0.25f, -0.25f,
+			0.25f, -0.25f, -0.25f,
+
+			0.25f, -0.25f, -0.25f,
+			0.25f,  0.25f, -0.25f,
+			-0.25f,  0.25f, -0.25f,
+
+			0.25f, -0.25f, -0.25f,
+			0.25f, -0.25f,  0.25f,
+			0.25f,  0.25f, -0.25f,
+
+			0.25f, -0.25f,  0.25f,
+			0.25f,  0.25f,  0.25f,
+			0.25f,  0.25f, -0.25f,
+
+			0.25f, -0.25f,  0.25f,
+			-0.25f, -0.25f,  0.25f,
+			0.25f,  0.25f,  0.25f,
+
+			-0.25f, -0.25f,  0.25f,
+			-0.25f,  0.25f,  0.25f,
+			0.25f,  0.25f,  0.25f,
+
+			-0.25f, -0.25f,  0.25f,
+			-0.25f, -0.25f, -0.25f,
+			-0.25f,  0.25f,  0.25f,
+
+			-0.25f, -0.25f, -0.25f,
+			-0.25f,  0.25f, -0.25f,
+			-0.25f,  0.25f,  0.25f,
+
+			-0.25f, -0.25f,  0.25f,
+			0.25f, -0.25f,  0.25f,
+			0.25f, -0.25f, -0.25f,
+
+			0.25f, -0.25f, -0.25f,
+			-0.25f, -0.25f, -0.25f,
+			-0.25f, -0.25f,  0.25f,
+
+			-0.25f,  0.25f, -0.25f,
+			0.25f,  0.25f, -0.25f,
+			0.25f,  0.25f,  0.25f,
+
+			0.25f,  0.25f,  0.25f,
+			-0.25f,  0.25f,  0.25f,
+			-0.25f,  0.25f, -0.25f
+		};
+
+		// Now generate some data and put it in a buffer object
+		glGenBuffers(1, &buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ARRAY_BUFFER,
+ 					sizeof(vertex_positions),
+					vertex_positions,
+					GL_STATIC_DRAW);
+		// Set up our vertex attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(0);
+
+		glEnable(GL_CULL_FACE);
+		glFrontFace(GL_CW);
+
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
     }
 
     virtual void render(double currentTime)
     {
-        static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
-        glClearBufferfv(GL_COLOR, 0, green);
+		// Clear the framebuffer with dark green
+		static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+		static const GLfloat one = 1.0f;
 
+		glViewport(0, 0, info.windowWidth, info.windowHeight);
+		glClearBufferfv(GL_COLOR, 0, green);
+		glClearBufferfv(GL_DEPTH, 0, &one);
+
+		// Activate our program
         glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Set the project matrix
+		glUniformMatrix4fv(proj_location, 1, GL_FALSE, proj_matrix);
+
+		// Set the model-view matrix
+		float f = (float)currentTime * (float)M_PI * 0.1f;
+		vmath::mat4 mv_matrix =
+			vmath::translate(0.0f, 0.0f, -4.0f) *
+			vmath::translate(sinf(2.1f * f) * 0.5f,
+				cosf(1.7f * f) * 0.5f,
+				sinf(1.3f * f) * cosf(1.5f * f) * 2.0f) *
+			vmath::rotate((float)currentTime * 45.0f, 0.0f, 1.0f, 0.0f) *
+			vmath::rotate((float)currentTime * 81.0f, 1.0f, 0.0f, 0.0f);
+
+		glUniformMatrix4fv(mv_location, 1, GL_FALSE, mv_matrix);
+
+		// Draw 6 faces of 2 triangles of 3 vertices each = 36 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 36);
     }
 
     virtual void shutdown()
     {
         glDeleteVertexArrays(1, &vao);
         glDeleteProgram(program);
+		glDeleteBuffers(1, &buffer);
     }
 
+	void onResize(int w, int h)
+	{
+		sb7::application::onResize(w, h);
+		//aspect = (float)info.windowWidth / (float)info.windowHeight;
+		aspect = (float)w / (float)h;
+		proj_matrix = vmath::perspective(50.0f,
+										aspect,
+										0.1f,
+										1000.0f);
+	}
+
 private:
-    GLuint          program;
-    GLuint          vao;
+	GLuint          program;
+	GLuint          vao;
+	GLuint          buffer;
+	GLint           mv_location;
+	GLint           proj_location;
+
+	float           aspect;
+	vmath::mat4     proj_matrix;
 };
 
-DECLARE_MAIN(singlepoint_app)
+DECLARE_MAIN(spinnycube_my_app)
