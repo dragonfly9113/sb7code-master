0c84d57ac95b3853b41867b5775904cc1d8c5156
Weilin Luo <william_luo2004@yahoo.com>
2017-06-15 15:42:07 -0700

    simpletexture: add debug messages

--- src/simpletexture_my/simpletexture_my.cpp
+++ src/simpletexture_my/simpletexture_my.cpp
@@ -1,72 +1,121 @@
 /*
- * Copyright © 2012-2015 Graham Sellers
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
+* Copyright © 2012-2015 Graham Sellers
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice (including the next
+* paragraph) shall be included in all copies or substantial portions of the
+* Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
 
 #include <sb7.h>
+#include <vmath.h>
+
+#include <string>
+static void print_shader_log(GLuint shader)
+{
+	std::string str;
+	GLint len;
+
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len != 0)
+	{
+		str.resize(len);
+		glGetShaderInfoLog(shader, len, NULL, &str[0]);
+	}
+
+#ifdef _WIN32
+	OutputDebugStringA(str.c_str());
+#endif
+}
+
+static const char * vs_source[] =
+{
+	"#version 420 core                                                              \n"
+	"                                                                               \n"
+	"void main(void)                                                                \n"
+	"{                                                                              \n"
+	"    const vec4 vertices[] = vec4[](vec4( 0.75, -0.75, 0.5, 1.0),               \n"
+	"                                   vec4(-0.75, -0.75, 0.5, 1.0),               \n"
+	"                                   vec4( 0.75,  0.75, 0.5, 1.0));              \n"
+	"                                                                               \n"
+	"    gl_Position = vertices[gl_VertexID];                                       \n"
+	"}                                                                              \n"
+};
+
+static const char * fs_source[] =
+{
+	"#version 430 core                                                              \n"
+	"                                                                               \n"
+	"uniform sampler2D s;                                                           \n"
+	"                                                                               \n"
+	"out vec4 color;                                                                \n"
+	"                                                                               \n"
+	"void main(void)                                                                \n"
+	"{                                                                              \n"
+	"    color = texture(s, gl_FragCoord.xy / textureSize(s, 0));                   \n"
+	"}                                                                              \n"
+};
+
+#if 0
+typedef void (APIENTRY *GLDEBUGPROC)(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar *message,
+	const void *userParam);
+#endif
+
+void APIENTRY simple_print_callback(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam)
+{
+	printf("Debug message with source 0x%04X, type 0x%04X, "
+		"id %u, severity 0x%0X, '%s'\n",
+		source, type, id, severity, message);
+}
+
+void initialize_debug_output()
+{
+	glDebugMessageCallback(&simple_print_callback, NULL);
+}
 
 class simpletexture_my_app : public sb7::application
 {
-    void init()
-    {
-        static const char title[] = "OpenGL SuperBible - Single Triangle";
+public:
+	void init()
+	{
+		static const char title[] = "OpenGL SuperBible - My Simple Texture";
 
-        sb7::application::init();
+		sb7::application::init();
 
-        memcpy(info.title, title, sizeof(title));
-    }
+		//info.flags.debug = 1;
 
-    virtual void startup()
-    {
-        static const char * vs_source[] =
-        {
-            "#version 420 core                                                 \n"
-            "                                                                  \n"
-            "void main(void)                                                   \n"
-            "{                                                                 \n"
-            "    const vec4 vertices[] = vec4[](vec4( 0.75, -0.75, 0.5, 1.0),  \n"
-            "                                   vec4(-0.75, -0.75, 0.5, 1.0),  \n"
-            "                                   vec4( 0.75,  0.75, 0.5, 1.0)); \n"
-            "                                                                  \n"
-            "    gl_Position = vertices[gl_VertexID];                          \n"
-            "}                                                                 \n"
-        };
+		memcpy(info.title, title, sizeof(title));
+	}
 
-		static const char * fs_source[] =
-		{
-			"#version 420 core                                                 \n"
-			"                                                                  \n"
-			"uniform sampler2D s;                                              \n"
-			"                                                                  \n"
-			"out vec4 color;                                                   \n"
-			"                                                                  \n"
-			"void main(void)                                                   \n"
-			"{                                                                 \n"
-			"    //color = vec4(0.0, 0.8, 1.0, 1.0);                            \n"
-			"	 //color = texelFetch(s, ivec2(gl_FragCoord.xy), 0);			\n"
-			"    color = texture(s, gl_FragCoord.xy / textureSize(s, 0));       \n"
-            "}                                                                 \n"
-        };
-
-		GLuint texture;
+	void startup(void)
+	{
+		// Setup debug message callback function
+		//initialize_debug_output();
 
 		// Generate a name for the texture
 		glGenTextures(1, &texture);
@@ -76,11 +125,11 @@ class simpletexture_my_app : public sb7::application
 
 		// Specify the amount of storage we want to use for the texture
 		glTexStorage2D(GL_TEXTURE_2D,   // 2D texture
-			1,               // 1 mipmap levels
+			8,               // 8 mipmap levels
 			GL_RGBA32F,      // 32-bit floating-point RGBA data
 			256, 256);       // 256 x 256 texels
 
-		// Define some data to upload into the texture
+							 // Define some data to upload into the texture
 		float * data = new float[256 * 256 * 4];
 
 		// generate_texture() is a function that fills memory with image data
@@ -95,41 +144,66 @@ class simpletexture_my_app : public sb7::application
 			GL_FLOAT,       // Floating point data
 			data);          // Pointer to data
 
-		// Free the memory we allocated before - \GL now has our data
+							// Free the memory we allocated before - \GL now has our data
 		delete[] data;
 
-        program = glCreateProgram();
-        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fs, 1, fs_source, NULL);
-        glCompileShader(fs);
+		program = glCreateProgram();
+		GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, 1, fs_source, NULL);
+		glCompileShader(fs);
 
-        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vs, 1, vs_source, NULL);
-        glCompileShader(vs);
+		print_shader_log(fs);
 
-        glAttachShader(program, vs);
-        glAttachShader(program, fs);
+		GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, 1, vs_source, NULL);
+		glCompileShader(vs);
 
-        glLinkProgram(program);
+		print_shader_log(vs);
 
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-    }
+		glAttachShader(program, vs);
+		glAttachShader(program, fs);
 
-    virtual void render(double currentTime)
-    {
-        static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
-        glClearBufferfv(GL_COLOR, 0, green);
+		glLinkProgram(program);
 
-        glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-    }
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
-    virtual void shutdown()
-    {
-        glDeleteVertexArrays(1, &vao);
-        glDeleteProgram(program);
-    }
+		// Get some information about the context
+		const GLchar *vendor = (const GLchar *)glGetString(GL_VENDOR);
+		const GLchar *renderer = (const GLchar *)glGetString(GL_RENDERER);
+		const GLchar *version = (const GLchar *)glGetString(GL_VERSION);
+
+		// Assemble a message
+		std::string message = std::string("Created debug context with ") +
+								std::string(vendor) + std::string(" ") +
+								std::string(renderer) +
+								std::string(". The OpenGL version is ") +
+								std::string(version) + std::string(".");
+		
+		// Send the message to the debug out log
+		glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION,
+								GL_DEBUG_TYPE_MARKER,
+								0X4752415A,
+								GL_DEBUG_SEVERITY_NOTIFICATION,
+								-1,
+								message.c_str());
+	}
+
+	void shutdown(void)
+	{
+		glDeleteProgram(program);
+		glDeleteVertexArrays(1, &vao);
+		glDeleteTextures(1, &texture);
+	}
+
+	void render(double t)
+	{
+		static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, green);
+
+		glUseProgram(program);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
 
 private:
 	void generate_texture(float * data, int width, int height)
@@ -149,8 +223,9 @@ private:
 	}
 
 private:
-    GLuint          program;
-    GLuint          vao;
+	GLuint      texture;
+	GLuint      program;
+	GLuint      vao;
 };
 
-DECLARE_MAIN(simpletexture_my_app)
+DECLARE_MAIN(simpletexture_my_app);
